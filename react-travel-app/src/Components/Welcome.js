d94e4b971a983f533cd6a2c4994fd3cc18205f02
luuxe <luxelily@gmail.com>
2022-07-31 14:14:47 -0500

    added an array of random travel phrases

--- react-travel-app/src/Components/Welcome.js
+++ react-travel-app/src/Components/Welcome.js
@@ -1,17 +1,43 @@
 import { motion } from 'framer-motion'
-const Welcome = ({welcomeToggle, setWelcomeToggle, destinationsToggle, setDestinationsToggle }) => {
+import Loading from './Loading'
+import { useState, useEffect } from 'react'
 
+const travelSayings = [
+    'Pack your bags',
+    'Jet, Set, Go',
+    'Now boarding..',
+    'Catch flights, not feelings',
+    'Adventure is out there',
+    'Not all those who wander are lost',
+    'My happy place is a window seat',
+    'An adventure a day keeps the doctor away'
+]
+
+const phraseIndex = Math.floor(Math.random() * travelSayings.length)
+
+
+const Welcome = ({ welcomeToggle, setWelcomeToggle, destinationsToggle, setDestinationsToggle }) => {
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000)
+    }, [])
+
+    
     return (
-            <div className = 'Welcome-content'>
-                <p className='Welcome-text'>Not sure where to go next? Let us help.</p>
+        <>
+            {!loading ? 
+        <div className='Welcome-content'>
+                <p className='Welcome-text'>{travelSayings[phraseIndex]}</p>
 
                 <motion.button className='feeling-lucky-btn' whileTap={{ scale: 0.8 }} onClick={(() => {
                     setDestinationsToggle(!destinationsToggle);
                     setWelcomeToggle(!welcomeToggle)
                 })}>Find my next destination</motion.button>
-
-
             </div>
+            : <Loading /> }
+            </>
     );
 };
 
